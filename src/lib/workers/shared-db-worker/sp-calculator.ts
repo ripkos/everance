1c3f30711b0645c01b2348a3982d20320c4668f8
ripkos <75448759+ripkos@users.noreply.github.com>
2023-12-27 04:51:02 +0100

    further calculateSP refactor

--- src/lib/workers/shared-db-worker/sp-calculator.ts
+++ src/lib/workers/shared-db-worker/sp-calculator.ts
@@ -15,10 +15,10 @@ export type SkillData = {
 	level: string | null | undefined;
 };
 
-const invGroupsSkillCategoryID = 16;
-const dgmTypeAttributesPrimaryID = 180;
-const dgmTypeAttributesSecondaryID = 181;
-const dgmTypeAttributesModifierID = 275;
+const iGsSkillCategoryID = 16;
+const dTAsPrimaryID = 180;
+const dTAsSecondaryID = 181;
+const dTAsModifierID = 275;
 
 const sqrt32 = Math.sqrt(32);
 const powers: Map<string, number> = new Map<string, number>([
@@ -35,7 +35,7 @@ function levelToSP(level: string, modifier: number): number {
 	if (!prevLevelPower || !currentLevelPower) {
 		throw new Error('Only supports levels 1-5!');
 	}
-    const baseWithModifier = 250 * modifier;
+	const baseWithModifier = 250 * modifier;
 	const currentLevel = Math.round(baseWithModifier * currentLevelPower);
 	const prevLevel = Math.round(baseWithModifier * prevLevelPower);
 	return Math.round(currentLevel - prevLevel);
@@ -44,65 +44,50 @@ function levelToSP(level: string, modifier: number): number {
 export function calculateSP(inputString: string, invTypesArr: invTypes[]): SPCalculatorResponseDTO {
 	let sum = 0;
 	const returnArray: Array<SkillData> = [];
-	inputString
+	const inputStringMapped = inputString
 		.trimEnd()
 		.split('\n')
 		.map((str) => ({
 			name: str.substring(0, str.length - 2),
 			level: Number(str.substring(str.length - 1)).toFixed(0)
-		}))
-		.forEach((x, idx) => {
-			try {
-				const iT = invTypesArr.find(
-					(iT) =>
-						iT.invGroups?.categoryID === invGroupsSkillCategoryID &&
-						iT.typeName &&
-						iT.typeName === x.name
-				);
-				if (iT) {
-					const primary = iT.dgmTypeAttributes?.find(
-						(x) => x.attributeID === dgmTypeAttributesPrimaryID
-					)?.valueFloat;
-					const secondary = iT.dgmTypeAttributes?.find(
-						(x) => x.attributeID === dgmTypeAttributesSecondaryID
-					)?.valueFloat;
-					const modifier = iT.dgmTypeAttributes?.find(
-						(x) => x.attributeID === dgmTypeAttributesModifierID
-					)?.valueFloat;
-					const sp = levelToSP(x.level, modifier as number);
-					sum += sp;
-					returnArray.push({
-						order: idx,
-						id: iT.typeID,
-						name: iT.typeName,
-						primary: primary,
-						secondary: secondary,
-						sp: sp,
-						level: x.level
-					});
-				} else {
-					returnArray.push({
-						order: idx,
-						id: -1,
-						name: x.name,
-						level: x.level,
-						primary: null,
-						secondary: null,
-						sp: null
-					});
-				}
-			} catch (e) {
-				console.log(e);
+		}));
+	const processSkill = (x: { name: string; level: string }, idx: number) => {
+		try {
+			const iT = invTypesArr.find(
+				(iT) =>
+					iT.invGroups?.categoryID === iGsSkillCategoryID && iT.typeName && iT.typeName === x.name
+			);
+			if (iT?.dgmTypeAttributes) {
+				const dTA = iT.dgmTypeAttributes;
+				const primary = dTA.find((x) => x.attributeID === dTAsPrimaryID)?.valueFloat;
+				const secondary = dTA.find((x) => x.attributeID === dTAsSecondaryID)?.valueFloat;
+				const modifier = dTA.find((x) => x.attributeID === dTAsModifierID)?.valueFloat;
+				const sp = modifier ? levelToSP(x.level, modifier) : 0;
+				sum += sp;
 				returnArray.push({
 					order: idx,
-					id: -1,
-					name: x.name,
-					level: x.level,
-					primary: null,
-					secondary: null,
-					sp: null
+					id: iT.typeID,
+					name: iT.typeName,
+					primary: primary,
+					secondary: secondary,
+					sp: sp,
+					level: x.level
 				});
+				return;
 			}
+		} catch (e) {
+			console.log(e);
+		}
+		returnArray.push({
+			order: idx,
+			id: -1,
+			name: x.name,
+			level: x.level,
+			primary: null,
+			secondary: null,
+			sp: null
 		});
+	};
+	inputStringMapped.forEach(processSkill);
 	return { skills: returnArray, sum: sum };
 }
